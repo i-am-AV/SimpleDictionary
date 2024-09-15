2165f36e425f533514a63a73b496cb6a8d733e34
Alex Vasilyev <i-am-av@yandex.ru>
2024-09-15 22:55:20 +0300

    Initial project setup

---
+++ SimpleDictionary/WordListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WordListViewController.swift
+//  SimpleDictionary
+//
+//  Created by Alex Vasilyev on 15.09.2024.
+//
+
+import UIKit
+
+final class WordListViewController: UIViewController {
+
+    // MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    // MARK: - Private methods
+
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+    }
+}
