a2c268d62b6c91cf6d272b1e7d8c739265a91d71
Alex Vasilyev <i-am-av@yandex.ru>
2024-09-26 13:16:43 +0300

    Add fonts

---
+++ SimpleDictionary/BaseComponents/UI/Fonts.swift
@@ -0,0 +1,26 @@
+//
+//  Fonts.swift
+//  SimpleDictionary
+//
+//  Created by Alex Vasilyev on 26.09.2024.
+//
+
+import UIKit
+
+// swiftlint:disable nesting
+enum Font {
+    enum WordListScreen {
+        enum Cell {
+            /// Шрифт для вокабулы
+            static let title: UIFont = UIFont.systemFont(ofSize: 18, weight: .medium, width: .expanded)
+            /// Шрифт для перевода
+            static let subtitle: UIFont = UIFont.systemFont(ofSize: 20, weight: .regular, width: .condensed)
+            /// Шрифт для транскрипции
+            static let subtitle2: UIFont = UIFont.systemFont(ofSize: 12, weight: .regular, width: .standard)
+        }
+        enum AddButton {
+            static let title: UIFont = UIFont.systemFont(ofSize: 20, weight: .medium, width: .standard)
+        }
+    }
+}
+// swiftlint:enable nesting
